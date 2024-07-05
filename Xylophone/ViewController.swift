15f6eb7a775127014a5c4621b455eafa36003338
Pushkar <pushkar14.patil@gmail.com>
2024-07-05 17:41:24 +0530

    first commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var player:AVAudioPlayer!
+    @IBAction func keyPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3){
+            sender.alpha=0.5
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.3){
+            UIView.animate(withDuration: 0.3){
+                sender.alpha=1
+            }        }
+        let key=sender.titleLabel!.text!;
+        let url=Bundle.main.url(forResource: key, withExtension: "wav");
+        do {
+            player = try AVAudioPlayer(contentsOf: url!)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+
+    }
+  
+    
+}
